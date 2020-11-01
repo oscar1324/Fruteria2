ddda5c35a45a2701499305bea5de678905366f2d
oizquierdo873 <oizquierdo873@gmail.com>
2020-11-01 19:24:42 +0100

    Subida de la segunda funcion llamada enviar, donde la colocamos en el botón para relizar la lista final de la compra. Fromado por un bucle for en cual calcula la nota media y final, además de intentar aparecer todas las restantes frutas y la multiplicación de todas ellas

--- js/js.js
+++ js/js.js
@@ -19,3 +19,40 @@ function acumulador(numero){
         }
     }
 }
+
+// SEGUNDA FUNCION
+function envio(){
+    let divEscribir;
+    let divMas;
+    let contenidoFinal = " "; /* creo*/ 
+    let multiplica = 0;
+    divEscribir=document.getElementById("final");
+    divMas = document.getElementById("final");
+    for (var i = 0; i < 10; i++) {
+        if(cantidadFru[i] != 0){
+            let multiplica = cantidadFru[i]*precioFru[i];
+            contenidoFinal += "<p>" + nombrefru[i] + " --- "+ cantidadFru[i] + " Kilo " + " --- " + precioFru[i] + " €" + " --- " + parseFloat(multiplica) + " €";
+        }
+    }
+
+    // ticekt
+    contenidoFinal +="<p>Precio Medio: "+ parseInt(total/aux) + " € </p>";
+    contenidoFinal+="<p>Precio Total: " + total + " Kilo </p>";
+
+    let contenidoplus = " ";
+    divEscribir.innerHTML = contenidoFinal; // almacenar
+    contenidoplus = document.getElementById("final");
+    contenidoplus = papaya.getNombre() + " --- " + papaya.getKilos + " --- " + papaya.getKilos();
+    contenidoplus = frambuesa.getNombre() + " --- " + frambuesa.getKilos + " --- " + frambuesa.getKilos();
+    contenidoplus = arandanos.getNombre() + " --- " + arandanos.getKilos + " --- " + arandanos.getKilos();
+    contenidoplus = sandia.getNombre() + " --- " + sandia.getKilos + " --- " + sandia.getKilos();
+    contenidoplus = platanos.getNombre() + " --- " + platanos.getKilos + " --- " + platanos.getKilos();
+    contenidoplus = melocoton.getNombre() + " --- " + melocoton.getKilos + " --- " + melocoton.getKilos();
+    contenidoplus = mango.getNombre() + " --- " + mango.getKilos + " --- " + mango.getKilos();
+    contenidoplus = aguacate.getNombre() + " --- " + aguacate.getKilos + " --- " + aguacate.getKilos();
+    contenidoplus = nispero.getNombre() + " --- " + nispero.getKilos + " --- " + nispero.getKilos();
+    contenidoplus = guayaba.getNombre() + " --- " + guayaba.getKilos + " --- " + guayaba.getKilos();
+    divMas.innerHTML = divMas;
+    
+
+}
